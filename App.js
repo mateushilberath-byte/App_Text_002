8596b2a83a5d78c9192a1123617b16d734cd9408
Mateus Hilberath <mateuscostahilberath@gmail.com>
2026-03-06 20:25:32 -0300

    Feat: Adição de fontes em certos elementos Text

--- App.js
+++ App.js
@@ -6,16 +6,16 @@ function App() {
   <View style= {{flex: 1, padding: 40, backgroundColor: 'red', justifyContent: 'center'}}>
     <View style= {{height: '40%', width: '40%', backgroundColor: 'green', marginBottom: 30, marginTop: 10}}>
    <Text style= {{fontSize: 20, textAlign: 'center', color: 'white'}}>
-    <Text style= {{fontFamily:''}}>Eu sou o maior cantador de parabéns {'\n'}</Text>
+    <Text style= {{fontFamily:'Brush Script MT'}}>Eu sou o maior cantador de parabéns {'\n'}</Text>
     <Text style = {{fontWeight:'bold'}}>Eu vou bater o recorde de aniversários celebrados{'\n'}</Text>
     <Text style = {{fontStyle: 'italic'}}>2050 aniversários celebrados</Text>
     </Text>
     </View>
     <View style= {{ height: '40%', width: '40%', backgroundColor: 'white'}}>
     <Text style= {{fontSize: 17, color: 'blue', fontWeight: 'bold', textAlign: 'right'}}>
-      <Text>Eu sou o maior pagador de impostos do mundo{'\n'}</Text>
+      <Text style={{fontFamily: 'Bernard MT Condensed'}}>Eu sou o maior pagador de impostos do mundo{'\n'}</Text>
       <Text>Eu amo imposto, eu amo ser taxado {'\n'}</Text>
-      <Text>2050 impostos diferentes para eu pagar {'\n'}</Text>
+      <Text style={{letterSpacing: 2}}>2050 impostos diferentes para eu pagar {'\n'}</Text>
       <Text>2050 produtos diferentes taxados</Text>
     </Text>
     </View>
